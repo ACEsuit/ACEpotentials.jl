341f35986be4c0a3f81e75609c4891bf5cfea2cf
gelzinyte <elena.gelzinyte@gmail.com>
2022-02-01 00:45:06 +0000

    extra quick JSON reader

--- src/read_params.jl
+++ src/read_params.jl
@@ -2,10 +2,15 @@
 # ------------------------------------------
 #   Read ACE fit parameters from file 
 
+using JSON
+
+export json_to_params, yaml_to_params
+
 function yaml_to_params(filename::AbstractString)
     # return ace_params()
 end
 
 function json_to_params(filename::AbstractString)
-    # return ace_params()
+    # TODO: replace any missing values with defaults
+    return JSON.parsefile(filename)
 end
