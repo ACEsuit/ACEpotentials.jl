634763b2fcd695f0df75c189723b2ec7cbbf45cc
gelzinyte <elena.gelzinyte@gmail.com>
2022-03-09 11:08:59 +0000

    correct script to generate artifacts

--- scripts/generate_artifacts.jl
+++ scripts/generate_artifacts.jl
@@ -1,62 +1,24 @@
-
-using ACE1pack, Pkg
+using ACE1pack
+using ArtifactUtils
 using Pkg.Artifacts
-using SHA
-
-artifacts_toml = joinpath(pathof(ACE1pack)[1:end-16], "Artifacts.toml")
 
 
-function generate_artifact(label, tarname, url)
+function safe_add_artifact!(label, url, artifacts_toml)
     data_hash = artifact_hash(label, artifacts_toml)
-
     if data_hash == nothing || !artifact_exists(data_hash)
-        tarfile = download(url, @__DIR__() * "/" * tarname)
-        hash_ = create_artifact() do artifact_dir
-            cp(tarfile, joinpath(artifact_dir, tarname))
-        end
-        tarball_hash = archive_artifact(hash_, joinpath(tarfile))
-        bind_artifact!(artifacts_toml, label, hash_,
-                    download_info = [ (url, tarball_hash) ], lazy=true, force=true)
+        add_artifact!(artifacts_toml, label, url, lazy=true, force=true)
     end
 end
 
 
+artifacts_toml = joinpath(pathof(ACE1pack)[1:end-16], "Artifacts.toml")
 
 label = "TiAl_tiny_dataset"
-tarname = "TiAl_tiny.tar.gz"
-url = "https://github.com/gelzinyte/test_julia_artifacts/blob/main/$(tarname)?raw=true"
-generate_artifact(label, tarname, url)
+url = "https://github.com/gelzinyte/test_julia_artifacts/blob/main/TiAl_tiny.tar.gz?raw=true"
+safe_add_artifact!(label, url, artifacts_toml)
+
 
 label = "ACE1pack_test_files"
-tarname = "ACE1pack_test_files.tar.gz"
-url = "https://github.com/gelzinyte/test_julia_artifacts/blob/main/$(tarname)?raw=true"
-generate_artifact(label, tarname, url)
-
-
-#---
-# testing the artifacts
-
-using ACE1pack, Pkg
-using Pkg.Artifacts
-
-#---
-# mock training set
-
-tial_tar = joinpath(artifact"TiAl_tiny_dataset", "TiAl_tiny.tar.gz")
-run(`tar -xzvf $tial_tar`)
-tial = "TiAl_tiny.xyz"
-configs =  IPFitting.Data.read_xyz(tial;
-    energy_key = "energy",
-    force_key = "force",
-    virial_key = "virial")
-
-#---
-# test files
-
-test_files_tar = joinpath(artifact"ACE1pack_test_files", "ACE1pack_test_files.tar.gz")
-run(`tar -xzvf $test_files_tar`)
-
-errors_fname = "expected_fit_errors.json"
-errors_dict = load_dict(errors_fname)
-
+url = "https://github.com/gelzinyte/test_julia_artifacts/blob/main/ACE1pack_test_files.tar.gz?raw=true"
+safe_add_artifact!(label, url, artifacts_toml)
 
