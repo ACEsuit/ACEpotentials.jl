a071857a8269366b094ae49ffa3a5cf7a1ca5ee5
Chuck Witt <witt.chuck@gmail.com>
2023-06-23 10:20:28 +0100

    simplify.

--- src/ACE1pack.jl
+++ src/ACE1pack.jl
@@ -1,38 +1,24 @@
 module ACE1pack
 
-# load and reexport JuLIP, ACE1
-# also ArgParse for the command line script
 using Reexport 
-@reexport using JuLIP
 @reexport using ACE1
 @reexport using ACE1x
 @reexport using ACEfit
-@reexport using ArgParse
-export JuLIP, ACE1, ACEfit, Argparse
-
-# Convenience Layer 
+@reexport using JuLIP
+export ACE1, ACE1x, ACEfit, JuLIP
 
 include("atoms_data.jl")
-
 include("fit.jl")
-
 include("data.jl")
-
 include("basis.jl")
-
 include("model.jl")
-
 include("solver.jl")
-
 include("regularizer.jl")
-
 include("read_params.jl")
-
 include("export.jl")
-
 include("analysis.jl")
 
-# a little hack to load ACE1pack artifacts from anywhere? 
+# hack to load ACE1pack artifacts from anywhere? 
 using LazyArtifacts
 artifact(str) = (@artifact_str str)
 
