909d69b5eca2ee2ef312c706ee60ffe1af398d32
Chuck Witt <witt.chuck@gmail.com>
2023-08-18 17:42:50 +0100

    use example_dataset properly.

--- test/test_recompw.jl
+++ test/test_recompw.jl
@@ -3,7 +3,7 @@ using ACE1pack, Test
 @info("Checking recompute_weights")
 
 # generate data
-rawdata = ACE1pack.example_dataset("Si_tiny.xyz")
+rawdata, _, _ = ACE1pack.example_dataset("Si_tiny")
 datakeys = (energy_key="dft_energy", force_key="dft_force", virial_key="dft_virial")
 weights = Dict("default" => Dict("E"=>30.0, "F"=>1.0, "V"=>1.0))
 data = [AtomsData(at; datakeys..., weights = weights) for at in rawdata]
