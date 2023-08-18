14e89189ca5f12e5ba1e7f562c1e3d7d5bf238f6
Christoph Ortner <christohortner@gmail.com>
2023-08-17 21:15:44 -0700

    update dataset analysis tutorial

--- tutorials/dataset_analysis.jl
+++ tutorials/dataset_analysis.jl
@@ -34,21 +34,28 @@ plot(plt_rdf, plt_adf, layout = (2,1), size = (800, 400))
 
 
 # One way we can use these distribution functions is to look at fitted potentials relative to where data is given. But even before a potential is fitted we can illustrate some properties of the basis functions used in ACE1pack. E.g. we can illustrate why we have chosen the distance transforms.
-# First, we generate a default Si model. 
+# First, we generate a default Si model and a second one with modified transform.
 
-model = acemodel(elements = [:Si,], order = 3,   
-                 totaldegree = 10, rcut = r_cut );
+model1 = acemodel(elements = [:Si,], order = 3,   
+                  totaldegree = 10, rcut = r_cut )
 
-# We have a utility function `get_transforms` that extracts the transforms from the model. We can then plot the transform gradients. In regions of r space with high gradient we have higher resolution. We see that the transforms concentrate resolution near the nearest neighbour distance and ensure there is no resolution at all near ``r = 0``.
+model2 = acemodel(elements = [:Si,], order = 3,   
+                  totaldegree = 10, rcut = r_cut, 
+                  transform = (:agnesi, 2, 2) );
+
+# We have a utility function `get_transforms` that extracts the transforms from the model. We can then plot the transform gradients. In regions of r space with high gradient we have higher resolution. We see that the transforms concentrate resolution near the nearest neighbour distance and ensure there is no resolution at all near ``r = 0``. The transform for the second model distributes resolution much more evenly across the radial domain. 
 
 using ACE1.Transforms: transform_d 
-trans, trans2 = ACE1pack.get_transforms(model)
-t = trans[(:Si, :Si)]
+trans1, trans1_pair = ACE1pack.get_transforms(model1)
+trans2, trans2_pair = ACE1pack.get_transforms(model2)
+t1 = trans1[(:Si, :Si)]
+t2 = trans2[(:Si, :Si)]
 rp = range(0.0, r_cut, length = 200)
 
-plt_t = plot(rp, abs.(transform_d.(Ref(t), rp)), lw=3, 
-             xlabel = L"r [\AA]", label = L"|t'(r)|",
+plt_t = plot(rp, abs.(transform_d.(Ref(t1), rp)), lw=3, 
+             xlabel = L"r [\AA]", label = L"|t_1'(r)|",
              yticks = [], ylabel = "", xlims = (0, r_cut))
+plot!(plt_t, rp, abs.(transform_d.(Ref(t2), rp)), lw=3, label = L"|t_2'(r)")             
 vline!([rnn(:Si),], lw=2, label = L"r_{\rm nn}")
 
 plt_rdf = histogram(rdf[(:Si, :Si)], bins=100, label = "rdf", 
@@ -64,11 +71,11 @@ using LazyArtifacts
 tial_data, _, _ = ACE1pack.example_dataset("TiAl_tutorial")
 
 rdf = ACE1pack.get_rdf(tial_data, r_cut)
-plt_TiTi = histogram(rdf[(:Ti, :Ti)], bins=100, xlabel = "", 
+plt_TiTi = histogram(rdf[(:Ti, :Ti)], bins=100, xlabel = "", c = 1,  
          ylabel = "RDF - TiTi", label = "", yticks = [], xlims = (0, r_cut) )
-plt_TiAl = histogram(rdf[(:Ti, :Ti)], bins=100, xlabel = "", 
+plt_TiAl = histogram(rdf[(:Ti, :Ti)], bins=100, xlabel = "", c = 2, 
          ylabel = "RDF - TiAl", label = "", yticks = [], xlims = (0, r_cut) )
-plt_AlAl = histogram(rdf[(:Al, :Al)], bins=100, xlabel = L"r [\AA]", 
+plt_AlAl = histogram(rdf[(:Al, :Al)], bins=100, xlabel = L"r [\AA]", c = 3, 
          ylabel = "RDF - AlAl", label = "", yticks = [], xlims = (0, r_cut), )
 plot(plt_TiTi, plt_TiAl, plt_AlAl, layout = (3,1), size = (700, 700))
          
