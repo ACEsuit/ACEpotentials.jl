51f1ab72b98df7be354f2366d35efaa0cb3f1ef1
Documenter.jl <documenter@juliadocs.github.io>
2023-08-22 22:27:37 +0000

    build based on 93de0a6

--- previews/PR160/tutorials/committee.jl
+++ previews/PR160/tutorials/committee.jl
@@ -4,63 +4,34 @@ using Plots, ACEpotentials
 
 # ### Perform the fit
 
-# TODO: improve this artifact approach
-using Pkg.Artifacts
-_artifact_toml = pathof(ACEpotentials)[1:end-20]*"Artifacts.toml"
-ensure_artifact_installed("Si_tiny_dataset", _artifact_toml)
-_hash = artifact_hash("Si_tiny_dataset", _artifact_toml)
-_artifact = artifact_path(_hash)
+# load data
+train, _, _ = ACEpotentials.example_dataset("Si_tiny")
+data_keys = (energy_key = "dft_energy", force_key = "dft_force")
 
-data = data_params(
-    fname=joinpath(_artifact, "Si_tiny.xyz"),
-    energy_key="dft_energy", force_key="dft_force", virial_key="dft_virial")
-ace_basis = basis_params(
-    type="ace",
-    species=[:Si],
-    N=4,
-    maxdeg=12,
-    r0=2.35126,
-    radial=basis_params(type="radial", pin=2, pcut=2, rcut=5.5, rin=1.65))
-pair_basis = basis_params(
-    type="pair",
-    species=[:Si],
-    maxdeg=3,
-    r0=2.35126,
-    rcut=6.5,
-    pcut=1,
-    pin=0)
-basis = Dict("ace"=>ace_basis, "pair"=>pair_basis)
-e0 = Dict("Si"=>-158.54496821)
-weights = Dict(
-    "default" => Dict("E"=>30.0, "F"=>1.0, "V"=>1.0),
-    "liq" => Dict("E"=>10.0, "F"=>0.66, "V"=>0.25))
+# create model
+model = acemodel(elements = [:Si,], 
+                 order = 3,   
+                 totaldegree = 10,           
+                 rcut = 5.0)
 
-# Give a nonzero committee_size to the solver
+# create solver, setting a nonzero committee size
+# at present, the SVD factorization is required for committees
+solver = ACEfit.BLR(committee_size=10, factorization=:svd)
 
-solver = solver_params(type=:blr, committee_size=10, factorization=:svd)
-
-params = fit_params(
-    data = data,
-    basis = basis,
-    solver = solver,
-    e0 = e0,
-    weights = weights,
-    ACE_fname = "")
-
-results = fit_ace(params)
-ip_comm = results["IP_com"]
+# perform the fit
+acefit!(model, train; solver=solver, data_keys...);
 
 # ### Inspect the committee energies
 
 atoms = rattle!(bulk(:Si, cubic=true) * 2, 0.2)
-energy, energies = ACE1.co_energy(ip_comm, atoms)
+energy, energies = ACE1.co_energy(model.potential, atoms)
 plot(1:10, energy*ones(10), label="mean energy")
 plot!(1:10, energies, seriestype=:scatter, label="committee energies")
 
-# Committee forces
+# ### Committee forces
 
-@show ACE1.co_forces(ip_comm, atoms)
+@show ACE1.co_forces(model.potential, atoms)
 
-# Committee virial
+# ### Committee virial
 
-@show ACE1.co_virial(ip_comm, atoms)
+@show ACE1.co_virial(model.potential, atoms)
