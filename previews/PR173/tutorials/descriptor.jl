65184da73b5388922c59a7ab8d50202dfaeaa5e7
Documenter.jl <documenter@juliadocs.github.io>
2023-09-05 23:39:59 +0000

    build based on 16cfdb2

--- previews/PR173/tutorials/descriptor.jl
+++ previews/PR173/tutorials/descriptor.jl
@@ -12,9 +12,10 @@ dataset, _, _ = ACEpotentials.example_dataset("Si_tiny");
 
 # An ACE basis specifies a vector of invariant features of atomic environments and can therefore be used as a general descriptor.
 
-basis = ACE1x.ace_basis( elements = [:Si],
-                          order = 3,        # body-order - 1
-                          totaldegree = 8, ); 
+basis = ACE1x.ace_basis(elements = [:Si],
+                        rcut = 5.5,
+                        order = 3,        # body-order - 1
+                        totaldegree = 8);
 
 # Compute an averaged structural descriptor for each configuration.
 
@@ -28,10 +29,10 @@ end
 
 # Finally, extract the descriptor principal components and plot. Note the segregation by configuration type.
 
-descriptors1 = hcat(descriptors...)
-M = fit(PCA, descriptors1; maxoutdim=3, pratio=1)
-descriptors_trans = transform(M, descriptors1)
+descriptors = hcat(descriptors...)
+M = fit(PCA, descriptors; maxoutdim=3, pratio=1)
+descriptors_trans = transform(M, descriptors)
 p = scatter(
     descriptors_trans[1,:], descriptors_trans[2,:], descriptors_trans[3,:],
     marker=:circle, linewidth=0, group=config_types, legend=:right)
-plot!(p, xlabel="PC1", ylabel="PC2", zlabel="PC3", camera=(40,10))
+plot!(p, xlabel="PC1", ylabel="PC2", zlabel="PC3", camera=(20,10))
