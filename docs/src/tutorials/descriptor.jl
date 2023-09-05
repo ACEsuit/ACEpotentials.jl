69bb737e535cf54e2739a39675648263f9c4e117
Chuck Witt <witt.chuck@gmail.com>
2023-09-05 16:21:02 +0100

    tweak descriptor tutorial.

--- docs/src/tutorials/descriptor.jl
+++ docs/src/tutorials/descriptor.jl
@@ -12,9 +12,10 @@ dataset, _, _ = ACEpotentials.example_dataset("Si_tiny");
 
 # An ACE basis specifies a vector of invariant features of atomic environments and can therefore be used as a general descriptor.
 
-basis = ACE1x.ace_basis( elements = [:Si],
-                          order = 3,        # body-order - 1
-                          totaldegree = 8, ); 
+basis = ACE1x.ace_basis(elements = [:Si],
+                        rcut = 5.5,
+                        order = 3,        # body-order - 1
+                        totaldegree = 8);
 
 # Compute an averaged structural descriptor for each configuration.
 
@@ -34,4 +35,4 @@ descriptors_trans = transform(M, descriptors1)
 p = scatter(
     descriptors_trans[1,:], descriptors_trans[2,:], descriptors_trans[3,:],
     marker=:circle, linewidth=0, group=config_types, legend=:right)
-plot!(p, xlabel="PC1", ylabel="PC2", zlabel="PC3", camera=(40,10))
+plot!(p, xlabel="PC1", ylabel="PC2", zlabel="PC3", camera=(20,10))
