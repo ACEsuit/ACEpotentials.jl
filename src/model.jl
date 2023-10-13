336b9b5c13bf533a4c999827893760db704a5b26
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-10-13 15:13:35 -0700

    atomsbase rep_dimer support

--- src/model.jl
+++ src/model.jl
@@ -85,14 +85,20 @@ function acefit!(model::ACE1Model, raw_data;
 
    if repulsion_restraint 
       if eltype(data) == AtomsData
-         append!(data, _rep_dimer_data(model, weight = restraint_weight))
+         append!(data, _rep_dimer_data(model; weight = restraint_weight))
       else
-         error("Repulsion restraint not implemented for AtomsBase")
+         tmp = _rep_dimer_data_atomsbase(
+            model; 
+            weight = restraint_weight,
+            energy_key = Symbol(energy_key),
+            kwargs...
+            )
+         append!(data, tmp)
       end
    end
                   
    P = _make_prior(model, smoothness, prior)
-   # We need to this to allow control over new and old assembly
+   # We need this to allow control over new and old assembly
    A, Y, W = _dispatch_to_assebly(
       data, 
       model.basis;
@@ -194,10 +200,15 @@ end
 
 # ---------------- Implementation of the repulsion restraint 
 
-function _rep_dimer_data_atomsbase(model; weight=0.01, name_E_weight = "E...") 
+function _rep_dimer_data_atomsbase(
+   model;
+   weight=0.01, 
+   energy_key=:energy,
+   group_key=:config_type,
+   kwargs...
+) 
    zz = model.basis.BB[1].zlist.list
    restraints = [] 
-   restraint_weights = Dict("restraint" => Dict("E" => weight, "F" => 0.0, "V" => 0.0))
    B_pair = model.basis.BB[1] 
    if !isa(B_pair, ACE1.PolyPairBasis)
       error("repulsion restraints only implemented for PolyPairBasis")
@@ -221,19 +232,28 @@ function _rep_dimer_data_atomsbase(model; weight=0.01, name_E_weight = "E...")
       end
       env_rin = ACE1.evaluate(envfun, _rin)
 
-      # at = at_dimer(_rin, z1, z2)
-      # X = [ [0,0,0], [_rin, 0, 0] ] 
-      # Z = [z1, z2]
-      # cell = [ _rin+1 0 0; 0 1 0; 0 0 1]
-      # pbc = fff 
+      a1 = Atom(zz[1].z, zeros(3)u"Å")
+      a2 = Atom(zz[2].z, [_rin, 0, 0]u"Å")
+      cell = [ [_rin+1, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]u"Å"
+      boundary_conditions = [DirichletZero(), DirichletZero(), DirichletZero()]
+      data = FlexibleSystem([a1, a2], cell, boundary_conditions)
+      
+      # add weight to the structure
+      kwargs =[
+         energy_key => env_rin, 
+         group_key  => "restraint", 
+         :energy_weight => weight,
+      ]
+      data = FlexibleSystem(data; kwargs...)
 
-      # add weight to the structure 
+      push!(restraints, data)
+   end
 
-
+   return restraints
 end
 
 function _rep_dimer_data(model; 
-                         weight = 0.01;
+                         weight = 0.01
                          )
    zz = model.basis.BB[1].zlist.list
    restraints = [] 
