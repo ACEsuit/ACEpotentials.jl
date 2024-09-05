44a25d17e12386b82c1c5c2560e43c148af7a75f
Veronika Kondratenko <vekondra@r8-ortner2.math.ubc.ca>
2024-09-05 11:45:19 -0700

    Added saving results

--- scripts/runfit.jl
+++ scripts/runfit.jl
@@ -1,11 +1,11 @@
-# using Pkg
-# Pkg.activate(joinpath(@__DIR__(), "../"))
+using Pkg
+Pkg.activate(joinpath(@__DIR__(), "../"))
 
 using ACEpotentials, AtomsBase, AtomsBuilder, Lux, StaticArrays, LinearAlgebra, 
-      Unitful, Zygote, Optimisers, Folds, Printf, Optim, Random, JSON, ArgParse, ExtXYZ, Dates
+      Unitful, Zygote, Optimisers, Folds, Printf, Optim, Random, JSON, ArgParse, Dates, ExtXYZ
 
 M = ACEpotentials.Models
-rng = Random.GLOBAL_RNGq
+rng = Random.GLOBAL_RNG
 
 parser = ArgParseSettings(description="Fit an ACE potential from parameters file")
 @add_arg_table parser begin
@@ -38,10 +38,9 @@ calc_model = M.ACEPotential(model, ps, st)
 train = ExtXYZ.load(args_dict["data"]["in_data"]["train_file"])
 test = ExtXYZ.load(args_dict["data"]["in_data"]["test_file"])
 
-# TODO: should this not be extracted from the dictionary???
-data_keys = [:energy_key => "dft_energy",
-             :force_key => "dft_force",
-             :virial_key => "dft_virial"]
+data_keys = Dict(:energy_key => "dft_energy",
+                 :force_key => "dft_force",
+                 :virial_key => "dft_virial")
 weights = Dict("default" => Dict("E"=>30.0, "F"=>1.0, "V"=>1.0),
                "liq" => Dict("E"=>10.0, "F"=>0.66, "V"=>0.25))
         
@@ -63,4 +62,21 @@ err = Dict("train" => err_train, "test" => err_test)
 # => generate a dictionary => store as JSON
 
 
+function nested_namedtuple_to_dict(nt)
+    return Dict(k => isa(v, NamedTuple) ? nested_namedtuple_to_dict(v) : v for (k, v) in pairs(nt))
+end
 
+function save_results_to_file(args_dict, err, calc_model, filename)
+    model_params_dict = nested_namedtuple_to_dict(calc_model.ps)
+    results = Dict(
+        "args_dict" => args_dict,
+        "err" => err,
+        "model_parameters" => model_params_dict
+    )
+    open(filename, "w") do io
+        write(io, JSON.json(results, 4))
+    end
+    @info "Results saved to file: $filename"
+end
+
+save_results_to_file(args_dict, err, calc_model, "scripts/results.json")
