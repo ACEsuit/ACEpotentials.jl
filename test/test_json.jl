4550b6bc19def074be015042f99563103af0a465
CheukHinHoJerry <cheukhinhojerry@gmail.com>
2024-09-13 15:26:40 -0700

    rebuild and check project

--- test/test_json.jl
+++ test/test_json.jl
@@ -31,6 +31,30 @@ using JSON
 example_params = JSON.parsefile(joinpath(tmpproj, "example_params.json"))
 results = JSON.parsefile(joinpath(tmpproj, "example_params_results/results.json"))
 
+@info("Rebuild and check Project and Manifest file ")
+using TOML
+
+# load content
+project_content = results["project"]
+manifest_content = results["manifest"]
+
+proj_path = joinpath(tmpproj, "Project.toml")
+tmp_proj_path = joinpath(tmpproj, "Project_tmp.toml")
+manifest_path = joinpath(tmpproj, "Manifest.toml")
+tmp_manifest_path = joinpath(tmpproj, "Manifest_tmp.toml")
+
+# write file
+open(tmp_proj_path, "w") do io
+   TOML.print(io, project_content)
+end
+open(tmp_manifest_path, "w") do io
+   TOML.print(io, manifest_content)
+end
+
+@assert TOML.parsefile(tmp_proj_path) == TOML.parsefile(proj_path)
+@assert TOML.parsefile(tmp_manifest_path) == TOML.parsefile(manifest_path)
+
+
 @info("Clean up temporary project")
 run(`rm -rf $tmpproj`)
 
